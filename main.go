acb5f28b69f92057474b4aaf3f4d8a3bc781f825
kolaxy <vybornoff@outlook.com>
2023-11-06 17:10:36 +0300

    lucky ticker

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func calculate_sum(str string) (result int){
+	for _, letter := range str {
+		num, err := strconv.Atoi(string(letter))
+		if err == nil {
+			result += num
+		}
+	}
+	return 
+}
 
 func main() {
-	var a string
-	fmt.Scan(&a)
-	fmt.Println(string(a[0]))
+	var ticker_number string
+	fmt.Scan(&ticker_number)
+	first := string(ticker_number[:3])
+	second := string(ticker_number[3:])
+	res := (bool(calculate_sum(first) == calculate_sum(second)))
+	if res == true {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
 }
