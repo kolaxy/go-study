c49559512e43e95eccc914ea15b226d9bed2b421
kolaxy <vybornoff@outlook.com>
2023-11-06 15:27:03 +0300

    if -- case

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-// !() - nogic NOT
-// && () - logic  AND
-// || () - logic OR
 
 func main() {
-	a := 6
-	b := 6
-	if a < b {
-		fmt.Println("A is less than B")
-	} else if a > b {
-		fmt.Println("A is greater than B")
-	} else {
-		fmt.Println("A is equal to B")
+	var a int
+	fmt.Scan(&a)
+
+	switch a {
+		case 1: fmt.Println("Zero")
+		case 2: fmt.Println("Two")
+		case 3: fmt.Println("Three")
+		case 4: fmt.Println("Four")
+		case 5: fmt.Println("Five")
+		case 6: fmt.Println("Six")
+		case 7: fmt.Println("Seven")
+		case 8: fmt.Println("Eight")
+		case 9: fmt.Println("Nine")
+		case 10: fmt.Println("Ten")
+
 	}
 }
