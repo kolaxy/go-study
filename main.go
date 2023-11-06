d09c7142c32c9aad202da4e3a860f335e78f361e
kolaxy <vybornoff@outlook.com>
2023-11-06 16:45:51 +0300

    string task

--- main.go
+++ main.go
@@ -2,15 +2,7 @@ package main
 import "fmt"
 
 func main() {
-	var a int
+	var a string
 	fmt.Scan(&a)
-	a1 := a/100
-	a2 := a/10%10
-	a3 := a%10
-	switch {
-		case a1 != a2 && a2 != a3 && a1 != a3:
-			fmt.Println("YES")
-		case 1 == 1:
-			fmt.Println("NO")
-		}
+	fmt.Println(string(a[0]))
 }
