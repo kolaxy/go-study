0ef7d3d87aae6b003f43e06fd04ea257178a1d40
kolaxy <vybornoff@outlook.com>
2023-11-06 16:42:01 +0300

    str/int cases task

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import "fmt"
 func main() {
 	var a int
 	fmt.Scan(&a)
+	a1 := a/100
+	a2 := a/10%10
+	a3 := a%10
 	switch {
-	case a > 0:
-		fmt.Println("Число положительное")
-	case a < 0:
-		fmt.Println("Число отрицательное")
-	case a == 0:
-		fmt.Println("Ноль")
-	}
+		case a1 != a2 && a2 != a3 && a1 != a3:
+			fmt.Println("YES")
+		case 1 == 1:
+			fmt.Println("NO")
+		}
 }
