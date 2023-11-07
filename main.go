79427bd71c1807b7fdec45a94e8f00173f952b68
kolaxy <vybornoff@outlook.com>
2023-11-07 14:27:46 +0300

    while 0

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 import "fmt"
 
 func main() {
-	var count int
-	fmt.Scan(&count)
-	output := 0
-	for i := 0; i < count; i++ {
-		var temp int
-		fmt.Scan(&temp)
-		if (temp % 8 == 0) && (10 <= temp && temp <= 99){
-			output += temp
+	maxelement := 0
+	counter := 0
+	for i := 0; i <= 1; {
+		var put int
+		fmt.Scan(&put)
+		if put == 0 {
+			break
+		} else if put == maxelement {
+			counter++
+		} else if put > maxelement {
+			counter=1
+			maxelement = put
 		}
 	}
-	fmt.Println(output)
+	fmt.Println(counter)
 }
