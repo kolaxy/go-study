6db0e494591403f134520c76970ea95b0f72a258
kolaxy <vybornoff@outlook.com>
2023-11-07 13:12:13 +0300

    year

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
-import (
-	"fmt"
-	"strconv"
-)
 
-func calculate_sum(str string) (result int){
-	for _, letter := range str {
-		num, err := strconv.Atoi(string(letter))
-		if err == nil {
-			result += num
-		}
-	}
-	return 
-}
+import "fmt"
 
 func main() {
-	var ticker_number string
-	fmt.Scan(&ticker_number)
-	first := string(ticker_number[:3])
-	second := string(ticker_number[3:])
-	res := (bool(calculate_sum(first) == calculate_sum(second)))
-	if res == true {
+	var number int
+	fmt.Scan(&number)
+	if (number % 400 == 0) || (number % 4 == 0 && number % 100 != 0) {
 		fmt.Println("YES")
 	} else {
 		fmt.Println("NO")
