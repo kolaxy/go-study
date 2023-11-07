4e74c48515b86edcd6298045c3e96160c4e37b6e
kolaxy <vybornoff@outlook.com>
2023-11-07 18:03:04 +0300

    forloop array

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	a := [5]int{1, 2, 3, 4, 5}
-	for i := 0; i < len(a); i++ {
-		fmt.Println(a[i])
+	for idx, num := range a {
+		fmt.Printf("Element with %d index : %d\n", idx, num)
 	}
 }
