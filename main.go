6f878941b7dfa79fd13f80735bef04cdf68c1b7f
kolaxy <vybornoff@outlook.com>
2023-11-07 17:50:27 +0300

    formatting

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 import "fmt"
 
 func main() {
-	var a float64 = 100.123456
-	fmt.Printf("This number %f type %T", a, a)
-	var a1 byte = 's'
-	var a2 int = 1234
-	fmt.Printf("%q %b", a1, a2)
-	var b1 string = "123"
-	var b2 string = "1234"
-	fmt.Printf("%q \n%s", b1, b2)
+	var put float64
+	fmt.Scan(&put)
+	if put > 10000 {
+		fmt.Printf("%e", put)
+	} else if put > 0 {
+		fmt.Printf("%.4f", put * put)
+	} else {
+		fmt.Printf("число %.2f не подходит",put)
+	}
 }
