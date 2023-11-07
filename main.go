a902a79af2c7cf4085a107202eda33e3104f91a5
kolaxy <vybornoff@outlook.com>
2023-11-07 18:00:55 +0300

    forloop array by index

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
+
 import "fmt"
 
 func main() {
-	var numbers [5]int = [5]int{1, 2, 3, 4, 5}
-
-	fmt.Println(numbers[0]) // 1
-	fmt.Println(numbers[4]) // 2
-
-	numbers[0] = 87
-
-	fmt.Println(numbers[0]) // 87
+	a := [5]int{1, 2, 3, 4, 5}
+	for i := 0; i < len(a); i++ {
+		fmt.Println(a[i])
+	}
 }
