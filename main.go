30c9e5e589387d673c29475ac7a7b42ac6ceb4e1
kolaxy <vybornoff@outlook.com>
2023-11-07 17:56:11 +0300

    arrays

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
+
 import "fmt"
 
 func main() {
-	var put float64
-	fmt.Scan(&put)
-	if put > 10000 {
-		fmt.Printf("%e", put)
-	} else if put > 0 {
-		fmt.Printf("%.4f", put * put)
-	} else {
-		fmt.Printf("число %.2f не подходит",put)
-	}
+	var a [3]int = [3]int{1, 2, 3}
+	b := [3]int{1, 2, 3}
+	c := [...]int{1, 2, 3}
+	d := [3]int{1: 12}
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
 }
