a9f8a77246a215c1a0f475afe5fbddcdffdeb39d
kolaxy <vybornoff@outlook.com>
2023-11-08 12:43:33 +0300

    copy

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ import "fmt"
 
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 7}
-	a = append(a[0:2], a[3:] ...)
-	fmt.Println(a)
+	b := make([]int, 3, 3)
+	n := copy(b, a)
+	fmt.Printf("a = %v\n", a)                  // a = [1 2 3]
+	fmt.Printf("b = %v\n", b)                  // b = [1 2 3]
+	fmt.Printf("Скопировано %d элемента\n", n) // Скопировано 3 элемента
 	}
