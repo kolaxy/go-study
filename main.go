ca38eb59ea6e2f0c753f5dfe68ff12a0c00b1579
kolaxy <vybornoff@outlook.com>
2023-11-08 11:04:05 +0300

    append

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 import "fmt"
 
 func main() {
-	initialUsers := [8]string{"Bob", "Alice", "Kate", "Sam", "Tom", "Paul", "Mike", "Robert"}
-	users1 := initialUsers[2:6]
-	users2 := initialUsers[:4]
-	users3 := initialUsers[3:]
-
-	fmt.Println(users1, users2, users3)
+	initialUsers := []string{"Bob", "Alice", "Kate", "Sam", "Tom", "Paul", "Mike", "Robert"}
+	initialUsers = append(initialUsers, "NEW")
+	fmt.Println(initialUsers)
 }
