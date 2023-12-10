6a2dfe165a38ac79d375606f7a76711546454b4e
kolaxy <vybornoff@outlook.com>
2023-12-11 01:21:41 +0300

    digital root

--- main.go
+++ main.go
@@ -1,28 +1,10 @@
 package main
+
 import "fmt"
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	array := make([]int, n)
-	for i := 0; i < n; i++ {
-		var temp int
-		fmt.Scan(&temp)
-		array[i] = temp 
-	}
-	min_array := array[0]
-	for _, v := range array {
-		if v < min_array {
-			min_array = v
-		}
-	}
-
-	
-	count := 0
-	for i := 0; i < n; i++ {
-		if array[i] == min_array {
-			count++
-		}
-	}
-	fmt.Println(count)
+	var num int
+	fmt.Scan(&num)
+	result := 1 + ((num - 1) % 9)
+	fmt.Print(result)
 }
