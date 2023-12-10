4a80706163fabfce0ffd646d28cedb9400e90f68
kolaxy <vybornoff@outlook.com>
2023-12-10 18:33:12 +0300

    sum 3

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 import "fmt"
 
 func main() {
-	var count int
-	fmt.Scan(&count)
-	array := make([]int, count)
-	for i := 0; i < count; i++ {
-		var temp int
-		fmt.Scan(&temp)
-		array[i] = temp
-	}
-	counter := 0
-	for i := 0; i < count; i++ {
-		if array[i] > 0 {
-			counter += 1
-		}
-	}
-	fmt.Println(counter)
+	var num int
+	fmt.Scan(&num)
+	first := num / 100
+	second := num % 100 / 10
+	last := num % 10
+	sum := first + second + last
+	fmt.Print(sum)
 }
-
