c005562b75d5e2840b450689585daf3fd2734d1f
kolaxy <vybornoff@outlook.com>
2023-12-10 17:35:11 +0300

    n array

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1, 2, 3, 4, 5, 6, 7}
-	b := make([]int, 3, 3)
-	n := copy(b, a)
-	fmt.Printf("a = %v\n", a)                  // a = [1 2 3]
-	fmt.Printf("b = %v\n", b)                  // b = [1 2 3]
-	fmt.Printf("Скопировано %d элемента\n", n) // Скопировано 3 элемента
+	var count int
+	var ls []int
+	fmt.Scan(&count)
+	for i := 0; i < count; i++ {
+		var temp int
+		fmt.Scan(&temp)
+		ls = append(ls, temp)
 	}
+	fmt.Println(ls[3])
+}
+
