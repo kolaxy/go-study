31e997df50444508685e7ca7abf59db6a323419d
kolaxy <vybornoff@outlook.com>
2023-12-11 01:17:36 +0300

    array min value count

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 import "fmt"
 
 func main() {
-	var numcount int
-	counter := 0
-	fmt.Scan(&numcount)
-	for i := 0; i < numcount; i++ {
+	var n int
+	fmt.Scan(&n)
+	array := make([]int, n)
+	for i := 0; i < n; i++ {
 		var temp int
 		fmt.Scan(&temp)
-		if temp == 0 {
-			counter++
+		array[i] = temp 
+	}
+	min_array := array[0]
+	for _, v := range array {
+		if v < min_array {
+			min_array = v
 		}
 	}
-	fmt.Println(counter)
+
+	
+	count := 0
+	for i := 0; i < n; i++ {
+		if array[i] == min_array {
+			count++
+		}
+	}
+	fmt.Println(count)
 }
