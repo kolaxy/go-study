5cf69c5f5072a77df2a304158d3261dee80afdd8
kolaxy <vybornoff@outlook.com>
2023-12-10 17:41:14 +0300

    largest

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 import "fmt"
 
-func main() {
-	var count int
-	var ls []int
-	fmt.Scan(&count)
-	for i := 0; i < count; i++ {
-		var temp int
-		fmt.Scan(&temp)
-		ls = append(ls, temp)
+func main()  {
+	array := [5]int{}
+	var a int
+	for i:=0; i < 5; i++{
+		fmt.Scan(&a)
+		array[i] = a
 	}
-	fmt.Println(ls[3])
+	temp := array[0]
+	for i:=0; i < len(array); i++ {
+		if array[i] > temp {
+			temp = array[i]
+		}
+	}
+	fmt.Println(temp)
 }
-
