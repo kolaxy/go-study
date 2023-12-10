f21bf09f4575afd816a48ce7934b8ab0dc50b7d9
kolaxy <vybornoff@outlook.com>
2023-12-11 00:33:13 +0300

    hours minutes

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 import "fmt"
 
 func main() {
-	var num int
-	fmt.Scan(&num)
-	first := num / 100
-	second := num % 100 / 10
-	last := num % 10
-	sum := first + second + last
-	fmt.Print(sum)
+	var number int
+	fmt.Scan(&number)
+	hours := number / 3600
+	minutes := (number - hours * 3600) / 60
+	fmt.Printf("It is %d hours %d minutes.", hours, minutes)
 }
