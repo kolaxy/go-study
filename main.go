df3d3e12d50dbee346da39fe9b170e228abcb9e9
kolaxy <vybornoff@outlook.com>
2023-12-11 00:56:22 +0300

    average

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 import "fmt"
 
 func main() {
-	var a, b, c int
-	fmt.Scan(&a, &b, &c)
-	if a < b + c && b < a + c && c < a + b {
-		fmt.Printf("Существует") 
-	} else {
-		fmt.Printf("Не существует")
-	}
+	var a, b float64
+	fmt.Scan(&a, &b)
+	fmt.Print((a + b)/2)
 }
