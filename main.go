71fd10cdb44148122ff86557941db179b2b3e961
kolaxy <vybornoff@outlook.com>
2023-12-11 12:41:28 +0300

    several returns

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var a = add(4, 5)
-	var b = add (20, 6)
-	fmt.Println(a)
-	fmt.Println(b)
+	var age, name = add(4,5,"Tom", "Simpson")
+	fmt.Println(age)
+	fmt.Println(name)
 }
 
-func add(x, y int) int {
-	return x + y
+func add(x, y int, firstName, lastName string) (int, string) {
+	var z int = x + y
+	var fullName = firstName + " " + lastName
+	return z, fullName
 }
