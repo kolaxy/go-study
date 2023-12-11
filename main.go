89f5e249c570e8e0c4ac3c27e949c53fc16d0bb0
kolaxy <vybornoff@outlook.com>
2023-12-11 12:24:51 +0300

    multi type

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	add(4, 5)
-	add(20, 6)
+	add(1, 2, 3.4, 5.6, 1.2)
 }
 
-func add(x int, y int) {
-	var z = x + y
-	fmt.Println("x + y =", z)
+func add(x, y int, a, b, c float32) {
+    var z = x + y
+    var d = a + b + c
+    fmt.Println("x + y = ", z)
+    fmt.Println("a + b + c = ", d)
 }
