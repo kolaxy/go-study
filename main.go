b9a054634a6f939372307e83a0c87a219bc6924f
kolaxy <vybornoff@outlook.com>
2023-12-11 12:21:53 +0300

    cunstom func add

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	hello()
+	add(4, 5)
+	add(20, 6)
 }
 
-func hello() {
-	fmt.Println("Hello")
+func add(x int, y int) {
+	var z = x + y
+	fmt.Println("x + y =", z)
 }
