2703ca8658b5167bfe0e31a86d043d968be45d61
kolaxy <vybornoff@outlook.com>
2023-12-11 12:20:38 +0300

    func first

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var num string
-	var digit string
-	fmt.Scan(&num, &digit)
+	hello()
+}
 
-	for _, v := range num {
-		if string(v) != digit {
-			fmt.Printf("%c", v)
-		}
-	}
+func hello() {
+	fmt.Println("Hello")
 }
