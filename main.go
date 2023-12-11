44adc023e8547beaef58b0c55b7049decb23c3c5
kolaxy <vybornoff@outlook.com>
2023-12-11 13:17:51 +0300

    minimumFromFour

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	f("Hello")
+	minimumFromFour()
 }
 
-func f(text string){
-	fmt.Println(text)
+func minimumFromFour() int {
+	array := []int{}
+	for i := 0; i < 4; i++ {
+		var temp int
+		fmt.Scan(&temp)
+		array = append(array, temp)
+	}
+	min := array[0]
+	for i := 0; i < 4; i++ {
+		if array[i] < min {
+			min = array[i]
+		}
+	}
+	return min
 }
