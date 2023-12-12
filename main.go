0375e08922ed8cfc8fa994bb0f30a7d0cab13283
kolaxy <vybornoff@outlook.com>
2023-12-12 17:09:19 +0300

    shoot+ridebike

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
-	x1 := 1
-	x2 := 2
-	test(&x1, &x2)
+type Char struct {
+	On    bool
+	Ammo  int
+	Power int
 }
 
-// считайте что fmt уже импортирован и main объявлен
-func test(x1 *int, x2 *int) {
-	// здесь пишите ваш код
-	temp := *x1
-	*x1 = *x2
-	*x2 = temp
-	fmt.Println(*x1, *x2)
+func (c *Char) Shoot() bool {
+	if c.On == false || c.Ammo <= 0 {
+		return false
+	} else {
+		c.Ammo = c.Ammo - 1
+		return true
+	}
+}
+
+func (c *Char) RideBike() bool {
+	if c.On == false || c.Power <= 0 {
+		return false
+	} else {
+		c.Power = c.Power - 1
+		return true
+	}
+}
+
+func main() {
+	testStruct := &Char{}
 }
