0ee8d0e169b83c38e9ed3b35fdb5eed3b9c665e1
kolaxy <vybornoff@outlook.com>
2023-12-12 17:55:55 +0300

    polydrom

--- main.go
+++ main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"unicode"
+	// "strings"
+	// "unicode"
 	"bufio"
 	"os"
 )
 
 func main() {
 	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	text = text[:len(text)-1]
-	runedText := []rune(text)
-	if unicode.IsUpper(runedText[0]) && strings.HasSuffix(text, ".") {
-		fmt.Println("Right")
-	} else {
-		fmt.Println("Wrong")
+	flag := true
+	textRune := []rune(text)
+	for i := 0; i < len(textRune); i++ {
+		if textRune[i] != textRune[len(textRune)-i-1] {
+			flag = false
+		}
 	}
+	if flag == true {
+		fmt.Println("Палиндром")
+	} else {
+		fmt.Println("Нет")
+	}
+}
+	// text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	// text = text[:len(text)-1]
+	// runedText := []rune(text)
+	// if unicode.IsUpper(runedText[0]) && strings.HasSuffix(text, ".") {
+	// 	fmt.Println("Right")
+	// } else {
+	// 	fmt.Println("Wrong")
+	// }
 }
