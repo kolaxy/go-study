0b493db17598708698d90ead9462d7b939ad27ba
kolaxy <vybornoff@outlook.com>
2023-12-12 17:42:34 +0300

    string 1-cap,last-dot

--- main.go
+++ main.go
@@ -1,31 +1,20 @@
 package main
 
-import "fmt"
-
-type Char struct {
-	On    bool
-	Ammo  int
-	Power int
-}
-
-func (c *Char) Shoot() bool {
-	if c.On == false || c.Ammo <= 0 {
-		return false
-	} else {
-		c.Ammo = c.Ammo - 1
-		return true
-	}
-}
-
-func (c *Char) RideBike() bool {
-	if c.On == false || c.Power <= 0 {
-		return false
-	} else {
-		c.Power = c.Power - 1
-		return true
-	}
-}
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	"bufio"
+	"os"
+)
 
 func main() {
-	testStruct := &Char{}
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	text = text[:len(text)-1]
+	runedText := []rune(text)
+	if unicode.IsUpper(runedText[0]) && strings.HasSuffix(text, ".") {
+		fmt.Println("Right")
+	} else {
+		fmt.Println("Wrong")
+	}
 }
