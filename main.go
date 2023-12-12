a688126f3119fec8e904fb41fc6f30c475cb1bae
kolaxy <vybornoff@outlook.com>
2023-12-12 21:43:50 +0300

    LItBeoFLcSGBOFQxMHoIuDDWcqcVgkcRoAeocXO

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-func calculate(a, b float64) float64 {
-	return math.Sqrt(a*a + b*b)
-}
+import "fmt"
 
 func main() {
-	var a, b float64
-	fmt.Scan(&a, &b)
-	fmt.Println(calculate(a, b))
+	var put string
+	fmt.Scan(&put)
+	for index, line := range put {
+		if index == len(put)-1 {
+			fmt.Printf("%v", string(line))
+		} else {
+			fmt.Printf("%v*", string(line))
+		}
+	}
 }
