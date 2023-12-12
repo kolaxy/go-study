e6250d3a97824b8e93691442ed3c348c34c00271
kolaxy <vybornoff@outlook.com>
2023-12-12 18:15:10 +0300

    X and S

--- main.go
+++ main.go
@@ -2,33 +2,20 @@ package main
 
 import (
 	"fmt"
-	// "strings"
-	// "unicode"
-	"bufio"
-	"os"
+
+	"strings"
 )
 
 func main() {
-	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	flag := true
-	textRune := []rune(text)
-	for i := 0; i < len(textRune); i++ {
-		if textRune[i] != textRune[len(textRune)-i-1] {
-			flag = false
-		}
-	}
-	if flag == true {
-		fmt.Println("Палиндром")
-	} else {
-		fmt.Println("Нет")
-	}
-}
-	// text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	// text = text[:len(text)-1]
-	// runedText := []rune(text)
-	// if unicode.IsUpper(runedText[0]) && strings.HasSuffix(text, ".") {
-	// 	fmt.Println("Right")
-	// } else {
-	// 	fmt.Println("Wrong")
-	// }
+
+	x, s := "", ""
+
+	fmt.Scan(&x)
+
+	fmt.Scan(&s)
+
+	res := strings.Index(x, s)
+
+	fmt.Println(res)
+
 }
