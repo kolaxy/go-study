63183b2dc337a94aa24ef6938481c3b048e2ebaa
kolaxy <vybornoff@outlook.com>
2023-12-13 11:09:45 +0300

    map

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	var numString string
-	fmt.Scan(&numString)
-	for _, v := range numString {
-		val, _ := strconv.Atoi(string(v))
-		res := int(val) * int(val)
-		fmt.Printf("%d", res)
+	m := map[int]int{
+		12: 2,
+		1:  5,
 	}
+	delete(m, 12)
+	if value, inMap := m[1]; inMap {
+		fmt.Println(value) // 10
+	}
+	
+	if value, inMap := m[2]; inMap {
+		fmt.Println(value) // Условие не выполняется
+	}
+	fmt.Println(m[12])
+	fmt.Println(m[1])
 }
