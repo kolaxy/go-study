8945ed82857ad08538a8755582a663ebbc0da0c3
kolaxy <vybornoff@outlook.com>
2023-12-13 16:41:05 +0300

    citypopulation

--- main.go
+++ main.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func work(n int) int {
-	if n > 3 {
-	   time.Sleep(time.Millisecond * 500)
-	   return n + 1
-	} else {
-	   time.Sleep(time.Millisecond * 500)
-	   return n - 1
-	}
- }
 func main() {
-	cached := make(map[int]int)
-	for i := 0; i < 10; i++ {
-		var temp int
-		fmt.Scan(&temp)
-		val, ok := cached[temp]
-		if ok == true {
-			fmt.Printf("%v ", val)
-		} else {
-			loaded := work(temp)
-			cached[temp] = loaded
-			fmt.Printf("%v ", loaded)
+	groupCity := map[int][]string{
+		10:   []string{"Moscow", "Klinton", "kirovsk"}, // города с населением 10-99 тыс. человек
+		100:  []string{"CPB", "NVZ", "Briansk"},         // города с населением 100-999 тыс. человек
+		1000: []string{"KIpr", "Harkov", "Novosibirsk"}, // города с населением 1000 тыс. человек и более
+	}
+	cityPopulation := map[string]int{
+		"Moscow":     54,
+		"Klinton":     65,
+		"kirovsk":     789,
+		"CPB":         345,
+		"NVZ":         548,
+		"Briansk":     579,
+		"KIpr":        65468,
+		"Harkov":      78645,
+		"Novosibirsk": 57498,
+	}
+
+	for k := range cityPopulation {
+		array := groupCity[100]
+		flag := true
+		for _, string := range array {
+			if k == string {
+				flag = false
+			}
+		}
+		if flag {
+			delete(cityPopulation, k)
 		}
 	}
+	fmt.Println(cityPopulation)
 }
