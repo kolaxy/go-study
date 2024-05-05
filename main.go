8580a0c48ea44a1a0267b95d284dfaaa986278df
kolaxy <vybornoff@outlook.com>
2024-05-05 12:54:49 +0300

    returning

--- main.go
+++ main.go
@@ -1,38 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	groupCity := map[int][]string{
-		10:   []string{"Moscow", "Klinton", "kirovsk"}, // города с населением 10-99 тыс. человек
-		100:  []string{"CPB", "NVZ", "Briansk"},         // города с населением 100-999 тыс. человек
-		1000: []string{"KIpr", "Harkov", "Novosibirsk"}, // города с населением 1000 тыс. человек и более
-	}
-	cityPopulation := map[string]int{
-		"Moscow":     54,
-		"Klinton":     65,
-		"kirovsk":     789,
-		"CPB":         345,
-		"NVZ":         548,
-		"Briansk":     579,
-		"KIpr":        65468,
-		"Harkov":      78645,
-		"Novosibirsk": 57498,
-	}
-
-	for k := range cityPopulation {
-		array := groupCity[100]
-		flag := true
-		for _, string := range array {
-			if k == string {
-				flag = false
-			}
-		}
-		if flag {
-			delete(cityPopulation, k)
-		}
-	}
-	fmt.Println(cityPopulation)
+	fmt.Println("asds")
 }
