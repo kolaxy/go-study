dcb79e02893e4416832ed69e1c6841c0a64025bd
kolaxy <vybornoff@outlook.com>
2024-06-11 00:25:09 +0300

    format

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	m1 := make(map[int]int)
-	m2 := map[int]int{
-		12: 2,
-		1:  5,
-	}
-	fmt.Println(m1)
-	fmt.Println(m2)
+	var index int8 = 15
+	var bigIndex int32
+	bigIndex = int32(index)
+
+	fmt.Println(bigIndex)
+	fmt.Printf("%T \n", bigIndex)
+
+	var a int32 = 22
+	var b uint64
+	b = uint64(a)
+
+	fmt.Println(b)
+	fmt.Printf("%T \n", b)
 }
