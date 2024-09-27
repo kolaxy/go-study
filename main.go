bacfc3cfe7d26ceca20b13da2fa4e521119660a6
kolaxy <vybornoff@outlook.com>
2024-09-28 00:04:21 +0300

    first go file

--- main.go
+++ main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var a string
-	fmt.Scan(&a)
-
-	if isValidPassword(a) {
-		fmt.Println("Ok")
-	} else {
-		fmt.Println("Wrong password")
-	}
-}
-
-func isValidPassword(a string) bool {
-	for _, symb := range a {
-		if !isLatinLetterOrDigit(symb) {
-			return false
-		}
-	}
-	return len(a) >= 5
-}
-
-func isLatinLetterOrDigit(s rune) bool {
-	return (s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z') || (s >= '0' && s <= '9')
+	fmt.Println("Hello World")
 }
